151062b9a91c584a9f43d0d14b75aa0124487e43
NickK21 <dodfannk@gmail.com>
2025-12-26 02:12:33 -0800

    Add basic console flow

--- Blackjack.Console/Program.cs
+++ Blackjack.Console/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to Blackjack");
+Console.WriteLine("Press Enter to start...");
+Console.ReadLine();
+Console.WriteLine("Dealing...");
